511fea5d5123346b32a3754f478dbfa35b74f5f4
Bartosz Meglicki <bmegli@op.pl>
2019-01-29 21:17:35 +0100

    add copyright notice
    - there was already copyright information in the LICENSE file
    - MPL 2.0 information was added to library files

--- hve.c
+++ hve.c
@@ -1,3 +1,14 @@
+/*
+ * HVE Hardware Video Encoding C library imlementation
+ *
+ * Copyright 2019 (C) Bartosz Meglicki <meglickib@gmail.com>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ */
+
 #include "hve.h"
 
 // FFmpeg
